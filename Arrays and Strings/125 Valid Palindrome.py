27f7e742e8e451a798ddbd37ecaae37b44aed504
Saajan P Varghese <157048612+saajan376@users.noreply.github.com>
2025-04-09 22:47:38 +0530

    commit

--- Arrays and Strings/125 Valid Palindrome.py
+++ Arrays and Strings/125 Valid Palindrome.py
@@ -1,11 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        palindrome=[]
-        for i in s:
-            if i.isalnum():
-                palindrome.append(i.lower())
-        palindromestring=''.join(palindrome)
-        return palindromestring==palindromestring[::-1]
+        left=0
+        right=len(s)-1
+        while left<right:
+            if not s[left].isalnum():
+                left+=1
+                continue
+            if not s[right].isalnum():
+                right-=1
+                continue
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
 
 #the time complexity is O(n) because we're passing once through the string and ''.join(cleaned) also is O(n) and reversing string is also o(n)
-#the space complexity is O(n) for the palindrome list and the reversed copy
+#the space complexity is O(1) because we are not creating new strings or data structures
