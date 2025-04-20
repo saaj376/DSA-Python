5507b3531cacd8f5eee36b1d8d050d16c15bf346
saajan376 <saajan.varghese.2006@gmail.com>
2025-04-20 22:19:58 +0530

    commit

--- Arrays and Strings/42 trap rainwater.py
+++ Arrays and Strings/42 trap rainwater.py
@@ -2,17 +2,25 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         if not height:
             return 0
-        left, right = 0, len(height) - 1
-        max_left, max_right = height[left], height[right]
-        water = 0
-        while left < right:
-            if max_left < max_right:
-                left += 1
-                max_left = max(max_left, height[left])
-                water += max_left - height[left]
+        n=len(height)
+        water=0
+        peak=0
+        for i in range(n):
+            if height[i]>height[peak]:
+                peak=i
+        maxleft=0
+        for i in range(peak):
+            if height[i]>maxleft:
+                maxleft=height[i]
             else:
-                right -= 1
-                max_right = max(max_right, height[right])
-                water += max_right - height[right]
+                water+=maxleft-height[i]
+        maxright=0
+        for i in range(n-1,peak,-1):
+            if height[i]>maxright:
+                maxright=height[i]
+            else:
+                water+=maxright-height[i]
         return water
-#the time complexity is O(n) and the space complexity is O(1)
+    
+#the time complexity is O(n) because we're traversing through the list in finding the peak, going from left to peak and from right to peak.
+#the space complexity is O(1) because we're not using any extra space that grows with the input size.
