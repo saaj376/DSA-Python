6e0e7fb97946101e92ef96f7c44784698e8928f3
saajan376 <saajan.varghese.2006@gmail.com>
2025-04-22 22:51:07 +0530

    commit

---
+++ Arrays and Strings/709 to lowercase.py
@@ -0,0 +1,6 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+        
+#the time complexity is O(n) where n is the length of the string.
+#the space complexity is O(n) because a new string a length n is created.
