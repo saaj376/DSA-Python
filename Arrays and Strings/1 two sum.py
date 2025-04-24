9e5c72a485449f1475bbbe45ba163b390311149d
saajan376 <saajan.varghese.2006@gmail.com>
2025-04-24 10:59:07 +0530

    commit

--- Arrays and Strings/1 two sum.py
+++ Arrays and Strings/1 two sum.py
@@ -12,4 +12,8 @@ class Solution:
                 right-=1
 
 #the time complexity is O(nlogn) because we're sorting the list and then traversing it once.'
-#the space complexity is O(n) because we're storing the sorted list in a new variable.
+#the space complexity is O(n) because we're storing the sorted list in a new variable.'
+
+#alternate solution to this problem can be done using hashmaps
+#the brute force solution is to use two loops and then check if the sum is equal to the target.
+#the time complexity is O(n²) because we're using two loops and the space complexity is O(1) because we're not using any extra space.
