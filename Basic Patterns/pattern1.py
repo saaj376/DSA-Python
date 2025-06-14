7fc94f65cc20a05705ddf4da0f1eff53f734398d
Saajan Varghese <157048612+gundumani376@users.noreply.github.com>
2025-06-14 15:00:21 +0530

    commit

---
+++ Basic Patterns/pattern1.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print("*"*5)
+
+
+'''
+The sample Output is 
+*****
+*****
+*****
+*****
+*****
+'''
---
+++ Basic Patterns/pattern3.py
@@ -0,0 +1,14 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+
+
+'''
+The sample Output is
+1
+12
+123
+1234
+12345
+'''
