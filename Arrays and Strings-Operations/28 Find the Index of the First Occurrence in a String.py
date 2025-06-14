4041d70fb01b05d0107b61ccca1c25ec5bc4c31d
Saajan Varghese <157048612+gundumani376@users.noreply.github.com>
2025-06-14 14:52:28 +0530

    commit

--- Arrays and Strings-Operations/28 Find the Index of the First Occurrence in a String.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        if needle in haystack:
-            return haystack.index(needle)
-        else:
-            return -1
-        
-#the time complexity is O(n*m) where n is the length of haystack and m is the length of needle.
-#the space complexity is O(1) because we are not using any extra space.
