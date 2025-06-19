6006cad3f329160262c5ab6ab67329d9b7516a03
Saajan Varghese <157048612+gundumani376@users.noreply.github.com>
2025-06-19 19:59:06 +0530

    main

---
+++ Math/317 bulb switcher.py
@@ -0,0 +1,6 @@
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(n**0.5)
+    
+# The time complexity is O(1) since we are only performing a constant number of operations.
+# The space complexity is also O(1) since we are using a constant amount of space
